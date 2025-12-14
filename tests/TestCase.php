f9f6dcbf73be48fc9838cda27c146621f3790a60
Arturo Nuñez <arturonunez020@gmail.com>
2025-12-14 13:59:55 -0300

    test cache config

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,14 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if ($this->app->runningUnitTests()) {
+            // Configura paths temporales para tests
+            $this->app['config']->set('view.compiled', env('VIEW_COMPILED_PATH', '/tmp/views'));
+            $this->app['config']->set('cache.stores.file.path', env('CACHE_PATH', '/tmp/cache'));
+        }
+    }
 }
